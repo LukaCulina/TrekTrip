b02a98d9d351bfc5546b76724241c8aa450bfa91
Luka Čulina <luka.culina56@gmail.com>
2025-05-15 23:21:42 +0200

    Updated highlightedtripcard to accept trip without ratings

--- frontend/src/Components/HighlightedTripCard/HighlightedTripCard.js
+++ frontend/src/Components/HighlightedTripCard/HighlightedTripCard.js
@@ -5,22 +5,24 @@ import './HighlightedTripCard.css';
 
 const HighlightedTripCard = ({ trip }) => {
 
+    const ratings = trip?.ratings || [];
+
   const calculateAverageRating = (ratings) => {
     if (!ratings.length) return 0;
     const total = ratings.reduce((sum, rating) => sum + rating.rating, 0);
     return total / ratings.length;
   };
 
-  const averageRating = calculateAverageRating(trip.ratings);
+  const averageRating = calculateAverageRating(ratings);
 
   return (
     <div className="featured-trip-card">
-      <img src={trip && trip.images && trip.images.length > 0 ? process.env.PUBLIC_URL + trip.images[0].url : ''} alt={trip.title} className="trip-image" />
+      <img src={trip && trip.images && trip.images.length > 0 ? process.env.PUBLIC_URL + trip?.images[0].url : ''} alt={trip?.title} className="trip-image" />
       <div className="trip-info">
-        <h3>{trip.title}</h3>
-        <p>{trip.destination}</p>
+        <h3>{trip?.title}</h3>
+        <p>{trip?.destination}</p>
 
-        <p>{trip.description}</p>
+        <p>{trip?.description}</p>
           <Stack spacing={1}>
               <Rating className="rating" name="half-rating-read" value={averageRating} precision={0.1} readOnly />
           </Stack>
