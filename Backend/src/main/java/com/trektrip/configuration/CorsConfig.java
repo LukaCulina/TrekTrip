3da3ffbf87a7ae64b3b30d23ca5496a4a4ea2ed2
Luka Čulina <106603546+LukaCulina@users.noreply.github.com>
2026-04-08 11:55:08 +0200

    Update CorsConfig.java

--- Backend/src/main/java/com/trektrip/configuration/CorsConfig.java
+++ Backend/src/main/java/com/trektrip/configuration/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000", "https://LukaCulina.github.io","http://localhost:3003","http://localhost:3001")
+                        .allowedOrigins("https://trek-trip.vercel.app", "https://LukaCulina.github.io", "http://localhost:3000","http://localhost:3003","http://localhost:3001")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("Authorization", "Content-Type", "Accept")
                         .allowCredentials(true)
